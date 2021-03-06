71659dc5186bafc93fcd31ef8a00417728aef333
alois9866 <alois9866@gmail.com>
2021-03-06 19:01:29 +0300

    Add auto resizing

--- 03_ThreeWayAndTkinter/main.py
+++ 03_ThreeWayAndTkinter/main.py
@@ -1,5 +1,6 @@
 import tkinter
 import tkinter.messagebox
+from tkinter import N, E, W, S
 
 from fifteen import Fifteen
 
@@ -15,8 +16,19 @@ class Application(tkinter.Frame):
 
     def __init__(self, game: Fifteen):
         self.game = game
+
         tkinter.Frame.__init__(self, None)
-        self.grid(column=4, row=5)
+        self.grid(sticky=N + S + E + W)
+
+        root = self.winfo_toplevel()
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+
+        for i in range(4):
+            self.columnconfigure(i, weight=1)
+        for i in range(5):
+            self.rowconfigure(i, weight=1)
+
         self.create_widgets()
 
     def with_update(self, f, *args):
@@ -30,20 +42,17 @@ class Application(tkinter.Frame):
             self.game.reset()
 
     def create_widgets(self):
-        tkinter.Button(self, text='New',
-                       command=lambda: self.with_update(self.game.reset),
-                       width=2, height=2).grid(row=0, column=0)
-        tkinter.Button(self, text='Quit',
-                       command=self.quit,
-                       width=2, height=2).grid(row=0, column=3)
+        tkinter.Button(self, text='New', command=lambda: self.with_update(self.game.reset)) \
+            .grid(row=0, column=0, columnspan=2, sticky=N + S + E + W)
+        tkinter.Button(self, text='Quit', command=self.quit) \
+            .grid(row=0, column=2, columnspan=2, sticky=N + S + E + W)
         for x in range(4):
             for y in range(4):
                 def hit(x=x, y=y):
                     self.with_update(self.hit, x, y)
 
-                tkinter.Button(self, text=number_to_piece(self.game.state[x][y]),
-                               command=hit,
-                               width=2, height=2).grid(row=y + 1, column=x)
+                tkinter.Button(self, text=number_to_piece(self.game.state[x][y]), command=hit) \
+                    .grid(row=y + 1, column=x, sticky=N + S + E + W)
 
 
 def main():
